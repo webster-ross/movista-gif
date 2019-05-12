0bf80946bbb72264245312b956085f9dcabc27b3
Webster Ross III <webster.ross@me.com>
2019-05-12 09:39:01 -0400

    Refactor

--- data/giphy.js
+++ data/giphy.js
@@ -1,29 +1,26 @@
-async fetchGifs(term, page = 0) {
-  try {
-    const res = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=DkUlx2MPVS8JcGmSyLIgHWB5uBleQdKp&q=${term}&limit=20&offset=${page * 20}`)
+const limit = 20
 
-    if (res.ok) {
-      const {data} = await res.json()
-      const images = []
+export default {
+  async search(term, page = 0) {
+    try {
+      const res = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=DkUlx2MPVS8JcGmSyLIgHWB5uBleQdKp&q=${term}&limit=20&offset=${page * 20}`)
 
-      data.forEach(image => {
-        const still = image.images.original_still.url
-        const gif = image.images.original.url
-        images.push({still, gif})
-      })
+      if (res.ok) {
+        const {data} = await res.json()
+        const images = []
 
-      if (page) this.setState({images: this.state.images.concat(images)})
-      else this.setState({images, term, hasMore: data.length == 20})
+        data.forEach(image => {
+          const still = image.images.original_still.url
+          const gif = image.images.original.url
+          images.push({still, gif})
+        })
+
+        return images
+      }
+      else throw 'Connection Error'
     }
-    else {
-      console.error("BAD");
-      //TODO display error
+    catch (e) {
+      throw 'Connection Error'
     }
-  }
-  catch (e) {
-    console.error("BAD2");
-    console.error(e)
-    this.setState({hasMore: true})
-    //TODO display error
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import fetch from 'isomorphic-unfetch'
+import giphy from '../data/giphy'
 
 import Layout from '../components/layout'
 import SearchBar from '../components/searchbar'
@@ -12,50 +13,35 @@ export default class Index extends React.Component {
     this.loadMore = this.loadMore.bind(this)
   }
 
-  async fetchGifs(term, page = 0) {
-    try {
-      const res = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=DkUlx2MPVS8JcGmSyLIgHWB5uBleQdKp&q=${term}&limit=20&offset=${page * 20}`)
-
-      if (res.ok) {
-        const {data} = await res.json()
-        const images = []
-
-        data.forEach(image => {
-          const still = image.images.original_still.url
-          const gif = image.images.original.url
-          images.push({still, gif})
-        })
-
-        if (page) this.setState({images: this.state.images.concat(images)})
-        else this.setState({images, term, hasMore: data.length == 20})
-      }
-      else {
-        console.error("BAD");
-        //TODO display error
-      }
-    }
-    catch (e) {
-      console.error("BAD2");
-      console.error(e)
-      this.setState({hasMore: true})
-      //TODO display error
-    }
-  }
-
-  loadMore() {
-    // load additional images for a previous search
+  async loadMore() {
+    // load additional images for a previous search term
     if (this.state.term) {
       const page = this.state.page + 1
-      this.fetchGifs(this.state.term, page)
-      this.setState({page})
+
+      try {
+        const images = await giphy.search(this.state.term, page)
+        this.setState({images: this.state.images.concat(images), page})
+      }
+      catch (e) {
+        this.setState({hasMore: true})
+        alert(e)
+      }
     }
   }
 
-  search(term) {
+  async search(term) {
     // load images based on search term
     if (term) {
       this.setState({images: [], page: 0, term, hasMore: false})
-      this.fetchGifs(term)
+
+      try {
+        const images = await giphy.search(term)
+        this.setState({images, term, hasMore: images.length == 20})
+      }
+      catch (e) {
+        this.setState({hasMore: true})
+        alert(e)
+      }
     }
   }
 
